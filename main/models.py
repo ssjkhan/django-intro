016bf06ac82564727b98caed7e978f83ba58a5d6
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-12-09 13:39:37 -0500

    Modify Friend modelf or foods

--- main/models.py
+++ main/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 
 class Friend(models.Model):
-    name = models.TextField()
-    love = models.IntegerField()
-    hobbies = models.ManyToManyField("Food", on_delete=models.CASCADE)
+    name = models.TextField(blank=False)
+    favFoods = models.ManyToManyField("Food", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"PK: {self.id} name: {self.name}"
