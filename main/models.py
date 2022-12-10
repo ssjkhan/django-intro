b083e9c5d98bc30aa47ea6d511e0ffa019a7a7d6
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-12-09 22:07:36 -0500

    add friend and food methods

--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.TextField(blank=False)
     favFoods = models.ManyToManyField("Food")
 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -17,7 +17,7 @@ def signup(req):
         form = UserCreationForm(req.POST)
         if form.is_valid():
             user = form.save()
-            login(req, user)
+            auth_login(req, user)
             return redirect('viewFriends')
         else:
             msg = "Invalid credentials - try again"
@@ -29,29 +29,60 @@ def signup(req):
     return render(req, 'registration/signup.html', context)
 
 def login(req):
-
     return render(req, 'registration/login.html')
 
 def home(req):
-    # return (req, )
     return render(req, 'home.html')
 
+@login_required
 def allFriends(req):
-    data = models.Friend.objects.all()
-    return render(req, 'friends/index.html')
+    friends = models.Friend.objects.filter(user = req.user)
+    favFoods = []
+    for friend in friends:
+        rest = list(models.Food.objects.exclude(id__in = friend.favFoods.all().values_list('id')).values())
+        favFoods.append(rest)
 
-def viewFriend(req):
-    return render(req, 'friends/detail.html')
-
-def addFriend(req):
-    return render(req, 'friends/detail.html')
-
-def allFoods(req):
-    return render(req, 'foods/index.html')
     
+    context = {'context' :zip(friends, favFoods)}
+    print(context['context'])
+    return render(req, 'friends/index.html', context)
 
+@login_required
+def viewFriend(req, friend_id):
+    friend = models.Friend.get(id=friend_id)
+    return render(req, 'friends/detail.html', {'friend':friend})
+
+
+class AddFriend(LoginRequiredMixin, CreateView):
+    model = models.Friend
+    fields = ['name', 'favFoods']
+    success_url = '/friends'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return  super().form_valid(form)
+
+@login_required
+def allFoods(req):
+    foods = models.Food.objects.all()
+    print(foods)
+    return render(req, 'foods/index.html', {'foods': foods})
+    
+@login_required
 def viewFood(req):
     return render(req, 'foods/detail.html')
+
+class AddFood(LoginRequiredMixin, CreateView):
+    model = models.Food
+    fields = ['name', 'flavour']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return  super().form_valid(form)
+    success_url = '/foods'
+
 
 def addFood(req):
     return render(req, 'foods/add.html')
